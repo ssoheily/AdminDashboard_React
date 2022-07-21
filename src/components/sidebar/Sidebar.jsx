81ea2d4fbc6fdb672568a35fe92d4b1921afa1f6
ssoheily <sam.soheil55@gmail.com>
2022-07-21 19:45:51 +0200

    created part Dashboard sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
-
+import "./Sidebar.css"
+import LineStyleIcon from "@mui/icons-material/LineStyle"
+import TimelineIcon from "@mui/icons-material/Timeline "
+import  TrendingUpIcon  from '@mui/icons-material/TrendingUp '
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+        <div className="sidebarWrapper">
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Dashboard</h3>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                            <LineStyleIcon className="sidebarIcon "> </LineStyleIcon>
+                            Home
+                    </li>    
+                    <li className="sidebarListItem">
+                            <TimelineIcon className="sidebarIcon "> </TimelineIcon>
+                            Analytics
+                    </li>    
+                    <li className="sidebarListItem">
+                            <TrendingUpIcon className="sidebarIcon "> </TrendingUpIcon>
+                            Sales
+                    </li>    
+                </ul> 
+            </div>
+        </div>
+    </div>
   )
 }
+ 

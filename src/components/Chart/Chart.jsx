b230cf2ff2a61f1fc0196371b22cea2015de4d9f
ssoheily <sam.soheil55@gmail.com>
2022-07-25 12:01:10 +0200

    install recharts and  create chart.js

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
-export default function Chart() {
+import {ResponsiveContainer , LineChart , Line , XAxis , CartesianGrid , Tooltip} from "recharts"
+export default function Chart({title, data, dataKey, grid}) {
   return (
-    <div></div>
+    <div className='chart'>
+         <h3 className="chartTitle" > {title}</h3>
+                   {/* acpect is for size charts */}
+         <ResponsiveContainer width="100%" aspect={4}>
+                <LineChart data={ data}>
+                     <XAxis dataKey="name" stroke='#5550bd' />
+                  <Line dataKey={dataKey} stroke='#5550bd' />
+                    <Tooltip />{/* in Hover */}
+                    {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="10" ></CartesianGrid>}
+                </LineChart>
+         </ResponsiveContainer>
+    </div>
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
  import React from 'react'
  import "./Home.css"
  import Features from "./../../components/features/Features"
- export default function Home() {
+ import Chart from "./../../components/Chart/Chart.jsx"
+ import {xAxisData} from "./../../datas"
+   export default function Home() {
    return (
         <div className="home">
           <Features></Features>
+          {/* grid is for backgroung charts ///show chart value in background =>dataKey="Sale" // */}
+          <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" ></Chart> 
         </div>
      
    )

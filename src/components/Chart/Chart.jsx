69c6d5d7f2b2be07ed9db377705b5f29a8989457
ssoheily <sam.soheil55@gmail.com>
2022-07-25 12:15:03 +0200

    created and finished  part  chart.js and css for chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Chart.css"
 import {ResponsiveContainer , LineChart , Line , XAxis , CartesianGrid , Tooltip} from "recharts"
 export default function Chart({title, data, dataKey, grid}) {
   return (
@@ -8,7 +9,8 @@ export default function Chart({title, data, dataKey, grid}) {
          <ResponsiveContainer width="100%" aspect={4}>
                 <LineChart data={ data}>
                      <XAxis dataKey="name" stroke='#5550bd' />
-                  <Line dataKey={dataKey} stroke='#5550bd' />
+                     {/*  type="monotone" : it is same border-radious for Line  */}
+                  <Line dataKey={dataKey} stroke='#5550bd'  type="monotone"/>
                     <Tooltip />{/* in Hover */}
                     {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="10" ></CartesianGrid>}
                 </LineChart>
--- src/datas.js
+++ src/datas.js
@@ -2,15 +2,15 @@ let  xAxisData =   [
     { "name":"jan", "Sale": 111_000},
     { "name":"feb", "Sale": 117_000},
     { "name":"mar", "Sale": 133_000},
-    { "name":"apr", "Sale": 116_000},
+    { "name":"apr", "Sale": 216_000},
     { "name":"may", "Sale": 126_000},
     { "name":"jun", "Sale": 122_000},
-    { "name":"jul", "Sale": 162_000},
+    { "name":"jul", "Sale": 562_000},
     { "name":"agu", "Sale": 112_000},
     { "name":"sept", "Sale":712_000},
-    { "name":"oct", "Sale": 119_000},
+    { "name":"oct", "Sale": 0},
     { "name":"nov", "Sale": 132_000},
-    { "name":"dec", "Sale": 146_000},
+    { "name":"dec", "Sale": 746_000},
 ]
 
 export {xAxisData} 
